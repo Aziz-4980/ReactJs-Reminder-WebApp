223e6c8f43a8576daad79c19110d8da1c4b5277d
Abdul Aziz Khan <31212073+aziz4980@users.noreply.github.com>
2022-09-23 19:24:58 +0500

    compomemt created

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import { Button } from 'react-bootstrap';
 import './App.css';
+import ReminderList from './components/ReminderList';
+import Reminder from './models/reminder';
+const reminders: Reminder[] =
+  [{ id: 1, title: "reminder" }]
+
 
 function App() {
   return (
-    <Button className="btn btn-primary "> Click me!</Button>
+    <ReminderList items={reminders}/>
   );
 }
 
